ae7f8d7ba547fa19e25f8823c9f38872f2f7cae0
mariesajan <mariesajan@gmail.com>
2016-09-02 16:59:27 -0700

    Code Cleanup

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-var express= require('express');
+var express = require('express');
 var app = express();
 var delimiters = require('./src/delimiters');
 var path = require('path');
 
 app.use('/', express.static(path.join("app", "public")));
 
-app.get('/', function(req, res, next){
-  res.sendFile(path.join(__dirname, "app","public", "index.html"));
+app.get('/', function(req, res, next) {
+  res.sendFile(path.join(__dirname, "app", "public", "index.html"));
 });
 
-app.get('/json', function(req, res, next){
+app.get('/json', function(req, res, next) {
   res.json(delimiters);
 });
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function() {
   console.log('Listening to port 3000....');
 });
